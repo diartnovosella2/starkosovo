e3a810094e06682be30c6819f66c6e0456af5535
starlabsDiart <diart@starlabspro.com>
2020-11-18 21:09:15 +0100

    Added new font style, added jobs design, fixed small css issues.

--- Includes/Enqueue.php
+++ Includes/Enqueue.php
@@ -9,21 +9,19 @@ class Enqueue extends _Enqueue
 
     public function admin_enqueue()
     {
-
         //styles admin
         wp_enqueue_style('mdlc_gig_style_admin', get_stylesheet_directory_uri() . '/assets/dist/admin.css', array(), $this->cache_breaker('/assets/dist/admin.css'));
-
         //scripts admin
         wp_enqueue_script('mdlc_gig_script_admin', get_stylesheet_directory_uri() . '/assets/dist/admin.js', array('jquery'), $this->cache_breaker('/assets/dist/admin.js'));
     }
 
     public function front_enqueue()
     {
-
         //styles front
         wp_enqueue_style('mdlc_gig_style', get_stylesheet_directory_uri() . '/assets/dist/front.css', array(), $this->cache_breaker('/assets/dist/front.css'));
-
         //script front
         wp_enqueue_script('mdlc_gig_script', get_stylesheet_directory_uri() . '/assets/dist/front.js', array('jquery'), $this->cache_breaker('/assets/dist/front.js'), true);
+        // Fonts
+        wp_enqueue_style('mdlc_gig_fonts', get_stylesheet_directory_uri() . '/assets/sass/utilities/_fonts.scss', array(), $this->cache_breaker('/assets/sass/utilities/_fonts.scss'));
     }
 }
