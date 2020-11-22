f49046c2bc4cd1f8c67c9b1e745d232e455aa0d5
njaziu <45635776+njaziu@users.noreply.github.com>
2020-11-22 20:52:37 +0100

    addded 404

--- 404.php
+++ 404.php
@@ -1,6 +1,15 @@
 <?php
 
-get_header();
-
-
+get_header();?>
+<div class="page-404-container">
+    	<div class="fof">
+            <h1>Error 404. Page not found!</h1>
+            <div class="home-btn">
+                <a href="<?= home_url('/')?>">
+                    <span>Go to home</span>
+                </a>
+            </div>
+    	</div>
+</div>
+<?php 
 get_footer();
