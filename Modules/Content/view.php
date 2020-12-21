1318beb8e0d2567d267aa1b566a587676ee69784
starlabsDiart <diart@starlabspro.com>
2020-12-21 22:27:23 +0100

    Fixed some small bugs.

--- Modules/Content/view.php
+++ Modules/Content/view.php
@@ -12,6 +12,7 @@
     $styleClass      = ($chooseStyle === 'Get Started') ? "get__started pt-2 pt-md-5" : (( $chooseStyle === "Success Story" )  ? "success__story pt-2 pt-md-2" : "register");
     $styleClassTitle = ($chooseStyle === 'Get Started') ? "get__started" : (( $chooseStyle === "Success Story" )  ? "success__story" : "register");
     $imagePosition   = $data['choose_position'] == "Left" ? "row-reverse" : "row";
+    $columnReverse   = ($chooseStyle === 'Success Story') ? "flex-column-reverse" : "flex-column";
     $bgColor         = strlen($data['bg_color']) != 0 ? $data['bg_color'] : 'default' ;
     $toggleFooter    = strlen($data['footer_title']) != 0 ? "justify-content-between" : "justify-content-center";
     $toggleMargin    = strlen($data['footer_title']) != 0 ? "mt-2 mt-lg-5" : "mt-2";
@@ -19,27 +20,32 @@
 ?>
 
 <?php if($chooseStyle != 'Wysiwyg Editor'){ ?>
-<div class="content d-flex flex-column flex-md-<?= $imagePosition;?>">
+<div class="content d-flex <?= $columnReverse; ?> flex-md-<?= $imagePosition;?>">
     <div class="content__left <?= $styleClass;?> content__left d-flex flex-column <?= $toggleFooter; ?> n bg_<?= $bgColor; ?>">
         <?php if($chooseStyle === 'Get Started')  { ?>
-            <h2 class="content__left__title__<?= $styleClassTitle;?> px-2 px-lg-0 pl-0 pl-lg-5"><?= $data['title']; ?></h2>
-            <div class="d-flex flex-wrap px-2 px-lg-0 pl-0 pl-lg-5">
+            <h2 class="content__left__title__<?= $styleClassTitle;?> pl-0 pl-lg-5"><?= $data['title']; ?></h2>
+            <div class="d-flex flex-wrap pl-0 pl-lg-5">
                 <?php foreach ($data['forms'] as $object) : ?>
-                    <div class="content__left__form">
-                        <p class="content__left__form__title"><?= $object['title']; ?></p> 
-                        <p class="content__left__form__description"><?= $object['description']; ?></p> 
+                    <div class="content__left__form d-flex flex-column justify-content-between">
+                        <div>
+                            <p class="content__left__form__title"><?= $object['title']; ?></p> 
+                            <p class="content__left__form__description"><?= $object['description']; ?></p> 
+                        </div>
                         <a class="content__left__form__url" href="<?= $object["url"];?> ">See How</a>
                     </div>
                 <?php endforeach; ?>
             </div>
             <?php } else if($chooseStyle === 'Success Story') { ?>
-                <h3 class="content__left__topdesc__<?= $styleClassTitle; ?> d-flex justify-content-end pl-2 pl-lg-5 pr-3 mb-0">SUCCESS STORY</h3>
-                <div class="content__left__description px-2 px-lg-5 ">
+                <h3 class="content__left__topdesc__<?= $styleClassTitle; ?> d-none d-md-flex justify-content-end pl-2 pl-lg-5 pr-3 mb-0">SUCCESS STORY</h3>
+                <div class="content__left__description px-2 px-lg-5 pb-5">
                     <h2 class="content__left__title__<?= $styleClass;?> pr-0 pr-lg-5 mb-2 mb-lg-3"><?= $data['title']; ?></h2>
-                    <?= $data['content_description'];?>
+                    <div class="">
+                        <?= $data['content_description'];?>
+                        <h3 class="content__left__topdesc__<?= $styleClassTitle; ?> d-flex justify-content-end pl-2 pl-lg-5 pr-3 mb-0 mt-0 d-block d-md-none float-right">SUCCESS STORY</h3>
+                    </div>
                 </div>
         <?php } else {?>
-            <div class="content__left__description content__left__description__register px-2 px-lg-5 <?= $toggleMargin; ?>">
+            <div class="content__left__description content__left__description__register px-2 px-lg-5 <?= $toggleMargin; ?> mb-5 mb-lg-0">
                 <h2 class="content__left__title__<?= $styleClass;?> pr-0 pr-lg-5 mb-2 mb-lg-3"><?= $data['title']; ?></h2>
                 <?= $data['content_description'];?>
                     <button class="apply__button mt-3 <?= $buttonMargin; ?>"> 
@@ -56,7 +62,7 @@
         <?php } ?>
         <?php if (strlen($data['footer_title']) !== 0) { ?>        
             <div class="content__left__footer px-2 px-lg-0 pl-0 pl-lg-5 py-3">
-                <p class="content__left__footer__title">
+                <p class="content__left__footer__title <?php if($chooseStyle === 'Success Story') {?> d-flex flex-column flex-lg-row <?php } ?>">
                     <?= $data['footer_title']; ?> 
                     <a class="content__left__footer__desc" href="tel:<?= $data['footer_description']; ?>"><?= $data['footer_description']; ?></a>
                 </p>
@@ -64,7 +70,7 @@
         <?php } ?>
     </div>
 
-    <div class="content__right">
+    <div class="content__right text-center text-md-left">
         <img src="<?= $data['image']['url']; ?>" alt="<?= $data['image']['title']; ?>">
     </div>
 </div>
