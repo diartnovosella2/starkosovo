d2dd11994c01e9b3bfe0be86f2e46413bd178e4e
njaziu <45635776+njaziu@users.noreply.github.com>
2020-12-29 23:09:18 +0100

    hot ifxes

--- Modules/Team/view.php
+++ Modules/Team/view.php
@@ -11,7 +11,7 @@
     <div class="team__description text-center">
         <?= $data['description']; ?>
     </div>
-    <div class="team__members d-flex flex-wrap justify-content-around justify-content-lg-between my-5 w-100">
+    <div class="team__members d-flex flex-wrap justify-content-around justify-content-lg-between my-5">
         <?php foreach ($data['repeater'] as $object) { ?>
             <div class="team__members__single">
                 <img src="<?= $object['image']['url'];?>" alt="<?= $object['image']['name'];?>">
